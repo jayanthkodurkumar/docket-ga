6eccca40c0965970e272a04bb60e715f2324571f
jayanthkodurkumar <jayanthkk1998@gmail.com>
2024-07-07 13:31:41 -0400

    docker

---
+++ set1/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("API IS WORKING");
+});
+app.get("/users", (req, res) => {
+  res.json([
+    {
+      name: "jayanth",
+      age: 25,
+    },
+  ]);
+});
+
+app.listen(5500, () => {
+  console.log("Server is up and running");
+});
---
+++ set2/src/App.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3100/users")
+      .then((response) => response.json())
+      .then((data) => setUsers(data))
+      .catch((error) => console.error("Error fetching users:", error));
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Users</h1>
+        <ul>
+          {users.map((user) => (
+            <li key={user.id}>{user.name}</li>
+          ))}
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
